5e7081d8cd1740de19e12b7deab8c5c26dd52a74
Chan Jung <chanjung@Is-this-Dell.local>
2025-09-15 14:03:26 -0500

    CustomAlignmentHStack implemented

--- CustomAlignmentVStack/CustomAlignmentVStack/ContentView.swift
+++ CustomAlignmentVStack/CustomAlignmentVStack/ContentView.swift
@@ -9,24 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CustomAlignmentVStack {
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. I forgot to buy oat milk from my gorcery store today, in my defense, they rearranged the layout for the 4th time this month (lie). I just wanna visit, get what i want, and yeet out but nooooooo i have to go through mountain of sale items for me to grab it the last time smh")
+        VStack {
+            Text("CustomAlignmentHStack")
+            CustomAlignmentHStack {
+                ZStack {
+                    Rectangle().fill(Color.blue).frame(width: 70, height: 90)
+                    Text("70x90")
+                }
+                
+                Text("🥕 top")
+                    .preferredVAlignment(.top)
+                
+                Text("🥕 center")
+                    .preferredVAlignment(.center)
+                
+                Text("🥕 bottom")
+                    .preferredVAlignment(.bottom)
+            }
+            .border(Color.cyan, width: 2)
             
-            Text("🥕 leeding")
-                .preferredHAlignment(.leading)
-            
-            Text("center one")
-            
-            Text("trail")
-                .preferredHAlignment(.trailing)
-            
-            Image(systemName: "cloud.heavyrain.circle")
-                .resizable()
-                .frame(width: 30, height: 30)
-                .preferredHAlignment(.leading)
-                .offset(x: 12)
+            CustomAlignmentVStack {
+                Text("CustomAlignmentVStack")
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. I forgot to buy oat milk from my gorcery store today, in my defense, they rearranged the layout for the 4th time this month (lie). I just wanna visit, get what i want, and yeet out but nooooooo i have to go through mountain of sale items for me to grab it the last time smh")
+                
+                Text("🥕 leeding")
+                    .preferredHAlignment(.leading)
+                
+                Text("center one")
+                
+                Text("trail")
+                    .preferredHAlignment(.trailing)
+                
+                Image(systemName: "cloud.heavyrain.circle")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .preferredHAlignment(.leading)
+                    .offset(x: 12)
+            }
+            .border(Color.blue, width: 1)
         }
-        .border(Color.blue, width: 1)
     }
 }
 
--- CustomAlignmentVStack/CustomAlignmentVStack/CusomAlignmentHStack.swift
+++ CustomAlignmentVStack/CustomAlignmentVStack/CusomAlignmentHStack.swift
@@ -6,3 +6,72 @@
 //
 
 import Foundation
+import SwiftUI
+
+public struct CustomAlignmentHStack: Layout {
+    var alignment: VerticalAlignment = .center
+    var spacing: CGFloat = 8
+    
+    // returns a size proposed for the CustomAlignmentVStack view.
+    // iterates thru the child views, find out the largest width, and add heights + applicable spaing values
+    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        let sizes = subviews.map { $0.dimensions(in: proposal) }
+        
+        let maxHeight: CGFloat = sizes.map(\.height).max() ?? 0
+        let width: CGFloat = sizes.reduce(0) { partialResult, newSize in
+                partialResult + newSize.width + spacing
+        } - spacing  // removing the last added spacing
+        
+        return .init(width: width, height: maxHeight)
+    }
+    
+    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        var origin = bounds.origin
+        let sizes = subviews.map { $0.dimensions(in: ProposedViewSize(width: bounds.width, height: bounds.width)) }
+        
+        // individual view's size and the corrosponding view
+        for (viewSize, view) in zip(sizes, subviews) {
+            let preferredVAlignment = view[PreferredVAlignment.self] ?? .center
+            
+            let yOffset: CGFloat = {
+                switch preferredVAlignment {
+                case .top: return 0
+                case .bottom, .firstTextBaseline, .lastTextBaseline: return (bounds.height - viewSize.height)
+                case .center: return ((bounds.height - viewSize.height) / 2)
+                default:
+                    return 0
+                }
+            }()
+            
+            view.place(at: CGPoint(x: origin.x, y: origin.y + yOffset),
+                       proposal: ProposedViewSize(width: viewSize.width, height: viewSize.height))
+            
+            origin.x += viewSize.width + spacing
+        }
+    }
+}
+
+nonisolated struct PreferredVAlignment: LayoutValueKey {
+    static let defaultValue: VerticalAlignment? = nil
+}
+
+extension View {
+    public func preferredVAlignment(_ alignment: VerticalAlignment?) -> some View {
+        layoutValue(key: PreferredVAlignment.self, value: alignment)
+    }
+}
+#Preview {
+    CustomAlignmentHStack {
+        Rectangle().fill(Color.blue).frame(width: 50, height: 70)
+        
+        Text("🥕 top")
+            .preferredVAlignment(.top)
+        
+        Text("🥕 center")
+            .preferredVAlignment(.center)
+        
+        Text("🥕 bottom")
+            .preferredVAlignment(.bottom)
+    }
+    .border(Color.red)
+}
